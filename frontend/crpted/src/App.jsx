be430f0285699fd22c4e33edfe7873dcdf52568c
Brijesh Singh <coderrrx@gmail.com>
2024-11-09 03:00:36 +0530

    Fixed Small Bugs

--- frontend/crpted/src/App.jsx
+++ frontend/crpted/src/App.jsx
@@ -1,18 +1,21 @@
-
+import React from 'react'
 import './App.css'
-import {HomePage} from "./Pages/Home/HomePage"
-import {AboutPage} from "./Pages/About/AboutPage"
-import {ContactPage} from "./Pages/Contact/ContactPage"
-import {PortfolioPage} from "./Pages/Portfolio/PortfolioPage"
+import HomePage from "./Pages/Home/HomePage"
+// import {AboutPage} from "./Pages/About/AboutPage"
+// import {ContactPage} from "./Pages/Contact/ContactPage"
+// import {PortfolioPage} from "./Pages/Portfolio/PortfolioPage"
+import  NavBar  from './NavBar/NavBar'
 
 
 function App() {
 
   return (
     <div>
-      <HomePage></HomePage>
+      <NavBar/>
+      <HomePage/>
     </div>
-  )
+  );
 }
 
 export default App
+
